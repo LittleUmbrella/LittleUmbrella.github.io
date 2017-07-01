e50c8275d460a0de6d667989dc11d1e7a16bfb9b
littleumbrella <tobyrf@yahoo.com>
2017-07-01 01:40:05 -0700

    declutter adding missing files

---
+++ js/internal/circleverse/viewModels/SearchableViewModel.js
@@ -0,0 +1,29 @@
+﻿
+    eaf.util.namespace('circleverse.viewModel');
+
+    //alert('hi');
+
+    circleverse.viewModel.SearchableViewModel = (function () {
+
+
+
+        return new JS.Module("circleverse.viewModel.SearchableViewModel", {
+
+            initialize: function (object, parent, globalSettings, opts) {// (tracker, uri, templateUri, templateId, resultTemplateUri, callSpec, name, id, businessClass, opts) {
+                //include: [becu_org.ui.viewModel.baseModule],
+                var self = this;
+                
+                self.callSuper();
+                //properties
+
+                self.globalSettings = globalSettings;
+
+                self.childViewModels.push(new circleverse.viewModel.SearchViewModel(object, parent, globalSettings, opts));
+
+            }
+            
+
+
+        });
+    })();
+
